ea5140ce5da6aa7e65fafb406b15bb3610b4c711
Mark Probst <mark.probst@gmail.com>
2021-01-19 11:53:33 -0500

    Document

--- src/default-map.ts
+++ src/default-map.ts
@@ -1,12 +1,44 @@
+/**
+ * The read-only interface of [[DefaultMap]].
+ *
+ * @typeParam K The key type.
+ * @typeParam V The value type.
+ */
 export interface ReadonlyDefaultMap<K, V> extends ReadonlyMap<K, V> {
     get(k: K): V;
 }
 
+/**
+ * A `Map` that returns default values for keys that are not present.
+ *
+ * To map strings to `Set`s of numbers, for example, `DefaultMap` can
+ * be used like this:
+ *
+ * ```
+ * const m = new DefaultMap<string, Set<number>>(() => new Set());
+ * m.get("foo"); // returns an empty `Set`
+ * m.get("bar").add(1);
+ * m.get("bar"); // returns a `Set` containing `1`.
+ * ```
+ *
+ * @typeParam K The key type.
+ * @typeParam V The value type.
+ */
 export class DefaultMap<K, V> extends Map<K, V> implements ReadonlyDefaultMap<K, V> {
+    /**
+     *
+     * @param _defaultFunc Must return the default value for key `k`.  Will only be called when necessary.
+     * @param initKVPs An array of [key, value] arrays to initialize the map with.
+     */
     constructor(private readonly _defaultFunc: (k: K) => V, initKVPs?: readonly (readonly [K, V])[]) {
         super(initKVPs);
     }
 
+    /**
+     * Returns the value for `k`.  If `k` is not present, creates
+     * the default value via [[_defaultFunc]], sets that default
+     * as the value for `k` and returns it.
+     */
     public get(k: K): V {
         let v = super.get(k);
         if (v === undefined) {
@@ -16,6 +48,16 @@ export class DefaultMap<K, V> extends Map<K, V> implements ReadonlyDefaultMap<K,
         return v;
     }
 
+    /**
+     * Sets the value for `k` to `f(v)` where `v` is the previous
+     * value for `k`, or the default if not present.  These two lines
+     * are equivalent:
+     *
+     * ```
+     * m.set("foo", m.get("foo") + 1);
+     * m.update("foo", x => x + 1);
+     * ```
+     */
     public update(k: K, f: (v: V) => V): V {
         const v = f(this.get(k));
         this.set(k, v);
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,112 @@
-export function proveType<T>(_val: T) {
+/**
+ * Do nothing, but only compile if `_val` is of type `T`.
+ */
+export function proveType<T>(_val: T): void {
     // do nothing, just prove the compiler thinks the types match
 }
 
+/**
+ * Only compile if `_never` has type `never`.  If run, trace the
+ * `message` and return `result`.
+ */
 export function proveNever<T>(_never: never, message: string, result: T): never {
     console.trace(message);
     return result as never;
 }
 
+/**
+ * Throw an `Error` and trace `message`.
+ */
 export function panic(message: string = "This should not happen"): never {
     console.trace(message);
     throw new Error(message);
 }
 
+/**
+ * Assert that `fact` is `true`.  If the assertion fails, [[panic]]
+ * with `message`.
+ */
 export function assert(fact: boolean, message: string = "Assertion failed"): asserts fact {
     if (fact) return;
     return panic(message);
 }
 
-export function assertNever(_never: never): never {
-    return panic("Hell froze over");
+/**
+ * Only compile if `_never` has the type `never`.  If run, [[panic]]
+ * with `message`.
+ */
+export function assertNever(_never: never, message: string = "`never` happened"): never {
+    return panic(message);
 }
 
+/**
+ * If `v` is not `undefined`, return `v`.  Otherwise, [[panic]] with
+ * a message including `reason`.
+ */
 export function defined<T>(v: T | undefined, reason?: string): T {
-    if (v === undefined)
-        return panic(
-            "Value was undefined but should be defined" +
-                (reason !== undefined ? `, expected to be defined because: ${reason}` : "")
-        );
+    if (v === undefined) {
+        return panic("Value was undefined but should be defined" + (reason !== undefined ? ` because: ${reason}` : ""));
+    }
     return v;
 }
 
+/**
+ * The only thing this function does is to trace if the promise
+ * `p` rejects.  It is useful to explain to ESLint that `p` is
+ * intentionally not `await`ed.
+ */
 export function dontAwait(p: Promise<unknown>): void {
     p.catch(e => {
-        console.error("Unhandled error from unawaited promise", e);
+        console.trace("Unhandled error from unawaited promise", e);
         throw e;
     });
 }
 
+/**
+ * A promise that resolves after `ms` milliseconds.
+ */
 export function sleep(ms: number): Promise<void> {
     return new Promise<void>(resolve => {
         setTimeout(resolve, ms);
     });
 }
 
+/**
+ * If both `a` and `b` are `undefined`, return `undefined`.
+ * If exactly one of the two is `undefined`, return the other.
+ * If both are not `undefined`, return `f(a, b)`.
+ */
 export function reduceTwo<T>(a: T | undefined, b: T | undefined, f: (aa: T, bb: T) => T): T | undefined {
     if (a === undefined) return b;
     if (b === undefined) return a;
     return f(a, b);
 }
 
+/**
+ * Remove `readonly` from all properties of `T`.
+ */
 export type Writable<T> = { -readonly [P in keyof T]: T[P] };
 
+/**
+ * If `x` is undefined, return `undefined`.  Otherwise, return `f(x)`.
+ */
 export function definedMap<T, U>(x: T | undefined, f: (xx: T) => U): U | undefined {
     if (x === undefined) return undefined;
     return f(x);
 }
 
+/**
+ * Returns whether `obj` has `name` as its own property.
+ */
 export function hasOwnProperty<T extends string>(obj: unknown, name: T): obj is { [P in T]: unknown } {
     if (obj === undefined || obj === null) return false;
     return Object.prototype.hasOwnProperty.call(obj, name);
 }
 
+/**
+ * Map `f` over `iterable`, and return an array of all results
+ * that are not `undefined`.
+ */
 export function mapFilterUndefined<T, U>(iterable: Iterable<T>, f: (x: T, i: number) => U | undefined): U[] {
     const result: U[] = [];
     let i = 0;
